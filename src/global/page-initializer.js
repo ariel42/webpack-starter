bd101060b916af91b2af9f10ca4043c73246bc5b
Ariel <ariel42dev@gmail.com>
2019-05-26 02:59:33 +0300

    don't compile feature detection code if not needed

--- src/global/page-initializer.js
+++ src/global/page-initializer.js
@@ -6,20 +6,22 @@
 
 //// Here is a sample code that can be used, the important things are to keep in sync with the imports inside 
 //// dynamic-polyfills.js file, and to write a simple code that doesn't need any polyfills by itself:
-function isBrowserMissingFeatures() {
-    var webUrlApi = require('core-js/internals/native-url'); //Web URL and URLSearchParams APIs (URL depends on URLSearchParams)
+let isBrowserMissingFeatures = USE_DYNAMIC_POLYFILLS ?
+    function () {
+        var webUrlApi = require('core-js/internals/native-url'); //Web URL and URLSearchParams APIs (URL depends on URLSearchParams)
 
-    const neededFeatures = [webUrlApi, window.fetch /*, etc. */]; //KEEP IN SYNC with dynamic-polyfills.js
+        const neededFeatures = [webUrlApi, window.fetch /*, etc. */]; //KEEP IN SYNC with dynamic-polyfills.js
 
-    let missingFeatures = false; //we use simple code, that doesn't need polyfills by itself
-    for (var i = 0; i < neededFeatures.length; ++i) {
-        if (!neededFeatures[i]) {
-            missingFeatures = true;
-            break;
+        let missingFeatures = false; //we use simple code, that doesn't need polyfills by itself
+        for (var i = 0; i < neededFeatures.length; ++i) {
+            if (!neededFeatures[i]) {
+                missingFeatures = true;
+                break;
+            }
         }
-    }
-    return missingFeatures;
-}
+        return missingFeatures;
+    } 
+    : undefined;
 
 let initPageAndThen = USE_DYNAMIC_POLYFILLS ?
     function (callback) {
